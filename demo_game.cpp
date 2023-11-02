aa181f81d2534134b43b83bcbd4eaed0a888f338
Oscarwasoccupied <z_xw99@outlook.com>
2023-11-02 19:42:36 -0400

    Add game file  to the repository

---
+++ demo_game.cpp
@@ -0,0 +1,188 @@
+#include "fssimplewindow.h"
+#include <vector>
+#include <cmath>
+
+const int SOLDIER_SIZE = 30;
+const int ENEMY_RADIUS = 20;
+
+class Soldier {
+public:
+    int x, y, size;
+
+    Soldier(int x, int y, int size) : x(x), y(y), size(size) {}
+
+    void draw() {
+        glBegin(GL_QUADS);
+        glVertex2i(x, y);
+        glVertex2i(x + size, y);
+        glVertex2i(x + size, y + size);
+        glVertex2i(x, y + size);
+        glEnd();
+    }
+
+    void move(int dx) {
+        x += dx;
+        y -= 1; // Move the soldier upwards
+    }
+};
+
+class Enemy {
+public:
+    int x, y, radius;
+    bool isMoving;
+
+    Enemy(int x, int y, int radius) : x(x), y(y), radius(radius), isMoving(false) {}
+
+    void draw() {
+        glBegin(GL_POLYGON);
+        for(int i = 0; i < 360; i++) {
+            double angle = i * 3.14159 / 180;
+            double fx = x + cos(angle) * radius;
+            double fy = y + sin(angle) * radius;
+            glVertex2d(fx, fy);
+        }
+        glEnd();
+    }
+
+    void move(int soldierX, int soldierY) {
+        if (isMoving) {
+            if (x < soldierX) {
+                x += 1;
+            } else if (x > soldierX) {
+                x -= 1;
+            }
+            if (y < soldierY) {
+                y += 1;
+            } else if (y > soldierY) {
+                y -= 1;
+            }
+        }
+    }
+};
+
+class Wall {
+public:
+    int x1, y1, x2, y2;
+    int operation; // 0: add, 1: subtract, 2: multiply, 3: divide
+    bool isPassed; // flag to track if a soldier has passed through the wall
+
+    Wall(int x1, int y1, int x2, int y2, int operation) : x1(x1), y1(y1), x2(x2), y2(y2), operation(operation), isPassed(false) {}
+
+    void draw() {
+        glBegin(GL_LINES);
+        glVertex2i(x1, y1);
+        glVertex2i(x2, y2);
+        glEnd();
+    }
+
+    int performOperation(int numSoldiers) {
+        switch(operation) {
+            case 0: return numSoldiers + 2; // add 2 soldiers
+            case 1: return numSoldiers - 2; // subtract 2 soldiers
+            case 2: return numSoldiers * 2; // multiply by 2
+            case 3: return numSoldiers / 2; // divide by 2
+            default: return numSoldiers;
+        }
+    }
+};
+
+int main() {
+    FsOpenWindow(0, 0, 800, 600, 1);
+
+    std::vector<Soldier> soldiers;
+    soldiers.push_back(Soldier(100, 500, SOLDIER_SIZE));
+
+    std::vector<Enemy> enemies;
+    enemies.push_back(Enemy(400, 300, ENEMY_RADIUS));
+
+    std::vector<Wall> walls;
+    walls.push_back(Wall(100, 400, 390, 400, 0)); // Wall with operation 0 (add)
+    walls.push_back(Wall(410, 400, 700, 400, 1)); // Wall with operation 1 (subtract)
+
+    // In the main loop
+    while(FsInkey() != FSKEY_ESC) {
+        glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+        // Draw road
+        glBegin(GL_LINES);
+        glVertex2i(100, 0);
+        glVertex2i(100, 600);
+        glVertex2i(700, 0);
+        glVertex2i(700, 600);
+        glEnd();
+
+        FsPollDevice();  // Check for user input
+        auto key = FsInkey();
+        
+        if(FSKEY_ESC == key) // if the user press ESC key
+        {
+            break;  // Exit the game
+        }
+
+        // Draw and move soldiers
+        for(auto& soldier : soldiers) {
+            soldier.draw();
+            if(key == FSKEY_LEFT) {
+                soldier.move(-1); // Move the soldier to the left
+            } else if(key == FSKEY_RIGHT) {
+                soldier.move(1); // Move the soldier to the right
+            } else {
+                soldier.move(0); // Keep moving upwards
+            }
+        }
+
+        // Draw and move enemies
+        for(auto& enemy : enemies) {
+            enemy.draw();
+            if (soldiers.size() > 0) {
+                enemy.move(soldiers[0].x, soldiers[0].y);
+            }
+        }
+
+        // Check for collisions between soldiers and enemies
+        for (auto it = soldiers.begin(); it != soldiers.end(); ) {
+            bool isHit = false;
+            for (auto& enemy : enemies) {
+                if (abs(it->x - enemy.x) < it->size && abs(it->y - enemy.y) < enemy.radius) {
+                    isHit = true;
+                    break;
+                }
+                
+            }
+            if (isHit) {
+                it = soldiers.erase(it);
+            } else {
+                ++it;
+            }
+        }
+
+        // Draw walls
+        for(auto& wall : walls) {
+            wall.draw();
+        }
+
+        // Check if soldier passed a wall
+        for(auto& wall : walls) {
+            if(!wall.isPassed && soldiers.size() > 0 && soldiers[0].y < wall.y1 && soldiers[0].x >= wall.x1 && soldiers[0].x <= wall.x2) {
+                wall.isPassed = true; // set the flag to true
+                for(auto& enemy : enemies) {
+                    enemy.isMoving = true;
+                }
+                int numSoldiers = soldiers.size();
+                numSoldiers = wall.performOperation(numSoldiers);
+                while(soldiers.size() < numSoldiers) {
+                    int startX = soldiers[0].x + (soldiers.size() % 2 == 0 ? -SOLDIER_SIZE : SOLDIER_SIZE) * ((soldiers.size() + 1) / 2);
+                    soldiers.push_back(Soldier(startX, soldiers[0].y, SOLDIER_SIZE)); // add new soldiers at the same y position as the existing soldier
+                }
+                while(soldiers.size() > numSoldiers && soldiers.size() > 1) {
+                    soldiers.pop_back(); // remove soldiers from the end
+                }
+            }
+        }
+
+        FsSwapBuffers();
+        FsSleep(25);
+    }
+
+    return 0;
+}
